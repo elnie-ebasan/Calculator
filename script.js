de5e6c5fabf1883ab672d7ad6176b9dfeb9dc2b0
elnie-ebasan <eebasan43@gmail.com>
2022-12-22 16:15:29 +0800

    initialize calculator

--- script.js
+++ script.js
@@ -1,22 +1,26 @@
-let result = document.getElementById("result")
+let result = document.getElementById("display")
 
 function del() {
     result.value = result.value
     .slice(0, -1)
 }
 
-function clearScreen(){
+function limpyo(){
     result.value = "";
 }
 
 function display(num){
+    if ((result.value.indexOf('.') !== -1) && (num == '.')) return
     result.value += num
 }
 
 function calculate() {
-    var t = document.getElementById("result").value;
+
+    var t = document.getElementById("display").value;
     var q = eval(t);
-    document.getElementById("result").value = q;
+    document.getElementById("display").value = q;
+
+
 }
 
 
